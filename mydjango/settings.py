301d09baf59ec188e3a493b7c2833066b28e3553
姜枨文 <63992597+jiangcw08@users.noreply.github.com>
2020-04-20 14:22:34 +0800

    Update settings.py

--- mydjango/settings.py
+++ mydjango/settings.py
@@ -86,9 +86,9 @@ DATABASES = {
           'ENGINE': 'django.db.backends.mysql', #数据库引擎
           'NAME': 'md',                       #数据库名
           'USER': 'root',                       #用户名
-          'PASSWORD': 'root',                   #密码
-          'HOST': '',                           #数据库主机，默认为localhost
-          'PORT': '',                           #数据库端口，MySQL默认为3306
+          'PASSWORD': '1993',                   #密码
+          'HOST': 'localhost',                           #数据库主机，默认为localhost
+          'PORT': '3306',                           #数据库端口，MySQL默认为3306
           'OPTIONS': {
              'autocommit': True,
          }
